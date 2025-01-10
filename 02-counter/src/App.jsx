8480dfae35817ccdd5bff529045b4df60f3f364f
oktavyanha <oktavyanhardiyono@gmail.com>
2025-01-10 13:51:25 +0700

    create 02-counter using vite react

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [counter, setCounter] = useState(6); // useState(variable) will return [variable, method-carried-variable]
 
-  return (
+    const addValue = () => {
+      setCounter(counter + 1);
+    }
+    
+    const removeValue = () => {
+      setCounter(counter - 1);
+    }
+
+    return (
     <>
-      
+      <h1>React course</h1>
+      <h2>Counter Value : {counter} </h2>
+      <button onClick={addValue}> Add Value </button> {" "}
+      <button onClick={removeValue}> Remove Value </button>
+      <p> footer : {counter}</p>
     </>
   )
 }
