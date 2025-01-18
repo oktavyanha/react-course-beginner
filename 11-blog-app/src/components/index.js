7b26e9cf694f647a789da87f44d030d9b2d12764
oktavyanha <oktavyanhardiyono@gmail.com>
2025-01-18 16:08:42 +0700

    implement AddPost, EditPost, AllPosts, Home, Login, and Signup pages; integrate PostForm and PostCard components; fetch posts from appwrite service

---
+++ 11-blog-app/src/components/index.js
@@ -0,0 +1,9 @@
+import Login from "../pages/Login";
+import Signup from './Signup'
+
+
+
+export {
+  Login,
+  Signup
+}
--- 11-blog-app/src/pages/Login.jsx
+++ 11-blog-app/src/pages/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Login as LoginComponent} from '../components/Login'
+// use '{}' because we did not use 'export default'
+
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='py-8'>
+      <LoginComponent />
+    </div>
   )
 }
 
